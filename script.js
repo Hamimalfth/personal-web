c27bbedce9a235ffe71b6113d4aac7379152d7b7
Hamimalfth <muhammadhamimmuftihudaalfatih@gmail.com>
2024-03-13 09:37:25 +0700

    mengubah

---
+++ script.js
@@ -0,0 +1,9 @@
+
+if (screen.width < 1280) {
+    document.getElementById("demo").innerHTML = "Gagal";
+    document.getElementById("header").innerHTML = "Gagal";
+    document.getElementById("main").innerHTML = "Gagal";
+    alert("Web Ini tidak Bisa DIakses Melalui Smartphone")
+
+
+}
