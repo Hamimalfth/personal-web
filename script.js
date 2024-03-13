86ceb0dbac7756c0e2aa6d9a8186d192bc9b9999
Hamimalfth <142770009+Hamimalfth@users.noreply.github.com>
2024-03-13 13:58:04 +0700

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-
-if (screen.width < 1280) {
-    document.getElementById("header").innerHTML = "Gagal";
-    document.getElementById("main").innerHTML = "Gagal";
-    alert("Web Ini tidak Bisa DIakses Melalui Smartphone")
-
-
-}
